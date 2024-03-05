90423ec3b7192cdd0edf8b02349656b6e41d08bb
YehorBondarenko96 <135607282+YehorBondarenko96@users.noreply.github.com>
2024-03-05 13:12:05 +0200

    Update contactsSchemas.js

--- schemas/contactsSchemas.js
+++ schemas/contactsSchemas.js
@@ -8,11 +8,12 @@ export const createOrderDataSchema = Joi.object({
     phone: Joi.string().pattern(new RegExp(/^[\d()-]+$/)).required(),
     address: Joi.string().required()
     },
-    order: {
+    order: Joi.array().items(Joi.object({
         id: Joi.string().required(),
         name: Joi.string().required(),
+        quantity: Joi.string().pattern(/^\d+$/).required(),
         price: Joi.string().pattern(/^[0-9,]+$/).required(),
         dataWasAdded: Joi.string().pattern(new RegExp(/^\d+(\.\d+)?$/)).required(),
         image: Joi.string().required()
-    }
+    })).required()
 })
