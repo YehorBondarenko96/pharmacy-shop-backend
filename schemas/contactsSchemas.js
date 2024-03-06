382402f8e38b4d637f988d9d33dacd91cc7157cb
YehorBondarenko96 <135607282+YehorBondarenko96@users.noreply.github.com>
2024-03-06 20:02:37 +0200

    8.0.0-06.03.24

--- schemas/contactsSchemas.js
+++ schemas/contactsSchemas.js
@@ -1,11 +1,10 @@
 import Joi from "joi";
 
 export const createOrderDataSchema = Joi.object({
-    id: Joi.string().required(),
     user:{
     name: Joi.string().required(),
     email: Joi.string().pattern(new RegExp(/^[^\s@]+@[^\s@]+\.[^\s@]+$/)).required(),
-    phone: Joi.string().pattern(new RegExp(/^[\d()-]+$/)).required(),
+    phone: Joi.string().pattern(/^[+\-\d()]+$/).required(),
     address: Joi.string().required()
     },
     order: Joi.array().items(Joi.object({
@@ -13,7 +12,7 @@ export const createOrderDataSchema = Joi.object({
         name: Joi.string().required(),
         quantity: Joi.string().pattern(/^\d+$/).required(),
         price: Joi.string().pattern(/^[0-9,]+$/).required(),
-        dataWasAdded: Joi.string().pattern(new RegExp(/^\d+(\.\d+)?$/)).required(),
+        dataWasAdded: Joi.string().pattern(/^[0-9.]+$/).required(),
         image: Joi.string().required()
     })).required()
 })
